293dbe2a8b603e86d502b0be83192d7e72700c00
KoganTheDev <Yuvalkogan123@gmail.com>
2026-01-09 09:27:28 +0200

    Finish Course

--- HWs/Lab4/simplex/src/simplex_algorithm.c
+++ HWs/Lab4/simplex/src/simplex_algorithm.c
@@ -1,89 +1,165 @@
+// simplex_algorithm.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "simplex.h"
 
-void simplex_algorithm() {
-    int i, j, k, optimal_flag, enter_id, exiting_id, basis_i;
-    double min_criteria, current_criteria, norm_sq, reduced_cost;
-    int iteration = 1;
+#define MAX_ITERATIONS 10000 // Safety limit
+
+void simplex_algorithm()
+{
+    int i, j, k, optimal_flag, enter_id, exiting_id;
+    double min_value;
+    int count = 1;
+
+    // Steepest Edge variables
+    double min_steepest_val;
+    int best_k_index;
+    double current_norm_sq, current_norm, current_ratio;
 
     optimal_flag = 0;
 
-    // Main Simplex Loop
-    while (!optimal_flag) {
-        enter_id = -1;
-        min_criteria = 0; // We look for the most negative value
+    printf(" m = %d, n = %d\n", m, n);
 
-        // 1. SELECT ENTERING VARIABLE (Steepest Edge Rule)
-        for (j = 0; j < n; j++) {
-            // Check if variable j is non-basic (Reduced cost is in the objective row, e.g., A[m][j])
-            // In many implementations, reduced costs rd are in row 'm'
-            reduced_cost = A[m][j]; 
+    printf("\nbasis1:\n");
+    for(i = 0; i < m; i++)
+        printf(" %d ", basis[i]);
+    printf("\n");
 
-            if (reduced_cost < -1e-9) { // Candidate for entering
-                norm_sq = 0;
-                // Calculate the squared norm of the direction vector: 1 + ||B^-1 * Ak||^2
-                // The column A[i][j] for i=0..m-1 represents B^-1 * Ak in the tableau
-                for (i = 0; i < m; i++) {
-                    norm_sq += A[i][j] * A[i][j];
-                }
-                
-                // steepest descent criteria: rd / sqrt(1 + sum(d_i^2))
-                current_criteria = reduced_cost / sqrt(1 + norm_sq);
+    printf("A:\n");
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++)
+            printf(" %6.2lf ", A[i][j]);
+        printf("\n");
+    }
 
-                if (enter_id == -1 || current_criteria < min_criteria) {
-                    min_criteria = current_criteria;
-                    enter_id = j;
-                }
-            }
-        }
-
-        // Check for optimality
-        if (enter_id == -1) {
-            optimal_flag = 1;
-            printf("Optimal solution found at iteration %d\n", iteration);
+    while(optimal_flag == 0)
+    {
+        if (count > MAX_ITERATIONS) {
+            printf("\n!!! STOPPED: Max iterations reached !!!\n");
             break;
         }
 
-        // 2. SELECT EXITING VARIABLE (Ratio Test)
-        exiting_id = -1;
-        double min_ratio = -1;
+        printf("count = %d\n", count);
+        count++;
 
-        for (i = 0; i < m; i++) {
-            if (A[i][enter_id] > 1e-9) {
-                double ratio = A[i][n] / A[i][enter_id]; // A[i][n] is usually the RHS (b)
-                if (exiting_id == -1 || ratio < min_ratio) {
-                    min_ratio = ratio;
-                    exiting_id = i;
-                }
-            }
+        bublesort(basis, m);
+
+        printf("\nbasis2:\n");
+        for(i = 0; i < m; i++)
+            printf(" %d ", basis[i]);
+        printf("\n");
+
+        set_d(); 
+
+        printf("\nd:\n");
+        for(i = 0; i < n; i++)
+            printf(" %d ", d[i]);
+        printf("\n");
+
+        set_A_aux();
+
+        copy_submatrix(B, A_aux, 0, m, 0, m); 
+
+        inv_gaussian(BI, B, m); 
+
+        erase_epsilons_matrix(BI, m, m);
+
+        matrix_mult(BIA_aux, BI, A_aux, m, m, n);
+        erase_epsilons_matrix(BIA_aux, m, n);
+
+        printf("\nBIA_aux (I, B-1*D):\n");
+        for(i = 0; i < m; i++) {
+            for(j = 0; j < n; j++)
+                printf(" %6.2lf ", BIA_aux[i][j]);
+            printf("\n");
         }
 
-        if (exiting_id == -1) {
-            printf("Unbounded problem.\n");
-            return;
-        }
+        matrix_vector_mult(BIb, BI, b, m, m);
+        erase_epsilons_vector(BIb, m);
 
-        // 3. PIVOTING
-        // Update basis: basis[exiting_id] = enter_id
-        basis[exiting_id] = enter_id;
+        copy_submatrix(D, A_aux, 0, m, m, n - m); 
 
-        // Perform Gaussian elimination on the tableau
-        double pivot_val = A[exiting_id][enter_id];
-        for (j = 0; j <= n; j++) {
-            A[exiting_id][j] /= pivot_val;
-        }
+        compute_cb_cd();
 
-        for (i = 0; i <= m; i++) {
-            if (i != exiting_id) {
-                double factor = A[i][enter_id];
-                for (j = 0; j <= n; j++) {
-                    A[i][j] -= factor * A[exiting_id][j];
-                }
-            }
-        }
+        vector_matrix_mult(cbBI, cb, BI, m, m);
+        erase_epsilons_vector(cbBI, m);
+
+        vector_matrix_mult(cbBID, cbBI, D, m, n - m);
+        erase_epsilons_vector(cbBID, n - m);
+
+        vector_subtract(rd, cd, cbBID, n - m);
+        erase_epsilons_vector(rd, n - m);
+
+        printf("\nrd( cd - cbBID ):\n");
+        for(i = 0; i < (n - m); i++)
+            printf(" %6.2lf ", rd[i]);
+        printf("\n\n");
+
+        min_value = find_min_value(rd, n - m);
         
-        iteration++;
-    }
-}
+        if (min_value >= -0.000000001) {
+            optimal_flag = 1;
+        }
+        else
+        {
+            // --- Steepest Edge Algorithm ---
+            
+            best_k_index = -1;
+            min_steepest_val = 1e30; 
+
+            for (k = 0; k < (n - m); k++) {
+                
+                // Check only improving variables
+                if (rd[k] < -0.000000001) {
+                    
+                    current_norm_sq = 0.0;
+                    // Calculate norm of column vector (d_k)
+                    for (i = 0; i < m; i++) {
+                        double val = BIA_aux[i][m + k];
+                        current_norm_sq += val * val;
+                    }
+                    
+                    current_norm = sqrt(current_norm_sq); 
+                    if (current_norm < 1e-9) current_norm = 1e-9;
+
+                    // Steepest Edge Rule: rd / ||d||
+                    current_ratio = rd[k] / current_norm;
+
+                    if (current_ratio < min_steepest_val) {
+                        min_steepest_val = current_ratio;
+                        best_k_index = k;
+                    }
+                }
+            }
+
+            if (best_k_index != -1) {
+                // Map local index to global variable ID
+                enter_id = d[m + best_k_index];
+            } else {
+                enter_id = find_most_negative();
+            }
+            
+            // --- End Algorithm ---
+
+            exiting_id = find_exiting_id(BIA_aux, BIb, enter_id, n, m);
+            
+            int exiting_val = (exiting_id >= 0 && exiting_id < m) ? basis[exiting_id] : -1;
+
+            printf("\nenter_id  = %d,  exiting_id = %d,"
+                   " d[exiting_id] = %d\n", 
+                   enter_id, exiting_id, exiting_val);
+
+            printf("pivot: enter_id = %d, exiting_id = %d\n", enter_id, exiting_val);
+
+            basis[exiting_id] = enter_id;
+            
+            printf("\nbasis3:\n");
+            for(i = 0; i < m; i++)
+                printf(" %d ", basis[i]);
+            printf("\n");
+
+        } // else
+    } // while
+} // simplex_algorithm
--- HWs/Lab5/simplex_algorithm.c
+++ HWs/Lab5/simplex_algorithm.c
@@ -1,41 +1,45 @@
-﻿// simplex_algorithm.c
+// simplex_algorithm.c
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "simplex.h"
 
+#define MAX_ITERATIONS 10000 // Safety limit
+
 void simplex_algorithm()
 {
-    int i, j, k, optimal_flag, enter_id, exiting_id, itemp, basis_i;
-    double dtemp, min_value;
+    int i, j, k, optimal_flag, enter_id, exiting_id;
+    double min_value;
     int count = 1;
 
-    double min_score, current_score, norm_sq;
-    int best_k, col_idx;
+    // Steepest Edge variables
+    double min_steepest_val;
+    int best_k_index;
+    double current_norm_sq, current_norm, current_ratio;
 
     optimal_flag = 0;
 
     printf(" m = %d, n = %d\n", m, n);
 
     printf("\nbasis1:\n");
-    for (i = 0; i < m; i++)
+    for(i = 0; i < m; i++)
         printf(" %d ", basis[i]);
     printf("\n");
 
-
     printf("A:\n");
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++)
             printf(" %6.2lf ", A[i][j]);
         printf("\n");
-    } // for
+    }
 
-
-
-    while (optimal_flag == 0)
+    while(optimal_flag == 0)
     {
+        if (count > MAX_ITERATIONS) {
+            printf("\n!!! STOPPED: Max iterations reached !!!\n");
+            break;
+        }
 
         printf("count = %d\n", count);
         count++;
@@ -43,212 +47,119 @@ void simplex_algorithm()
         bublesort(basis, m);
 
         printf("\nbasis2:\n");
-        for (i = 0; i < m; i++)
+        for(i = 0; i < m; i++)
             printf(" %d ", basis[i]);
         printf("\n");
 
-
-        set_d();
-
+        set_d(); 
 
         printf("\nd:\n");
-        for (i = 0; i < n; i++)
+        for(i = 0; i < n; i++)
             printf(" %d ", d[i]);
         printf("\n");
 
-
         set_A_aux();
 
-        printf("\nA_aux (B, D):\n");
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n; j++)
-                printf(" %6.2lf ", A_aux[i][j]);
-            printf("\n");
-        } // for
+        copy_submatrix(B, A_aux, 0, m, 0, m); 
 
-
-        copy_submatrix(B, A_aux, 0, m, 0, m); // Set B
-
-        printf("\nB:\n");
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < m; j++)
-                printf(" %6.2lf ", B[i][j]);
-            printf("\n");
-        } // for  
-
-
-        inv_gaussian(BI, B, m); // BI = B-1  
+        inv_gaussian(BI, B, m); 
 
         erase_epsilons_matrix(BI, m, m);
 
-        printf("\nBI:\n");
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < m; j++)
-                printf(" %6.2lf ", BI[i][j]);
-            printf("\n");
-        } // for  
-
-
-        matrix_mult(BIA_aux, BI, A_aux,
-            m, m, n);
+        matrix_mult(BIA_aux, BI, A_aux, m, m, n);
         erase_epsilons_matrix(BIA_aux, m, n);
 
         printf("\nBIA_aux (I, B-1*D):\n");
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n; j++)
+        for(i = 0; i < m; i++) {
+            for(j = 0; j < n; j++)
                 printf(" %6.2lf ", BIA_aux[i][j]);
             printf("\n");
-        } 
-
-        printf("\nA_aux (B,D):\n");
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n; j++)
-                printf(" %6.2lf ", A_aux[i][j]);
-            printf("\n");
         }
-
-
-        printf("b:\n");
-        for (i = 0; i < m; i++)
-            printf(" %6.2lf ", b[i]);
-
-
 
         matrix_vector_mult(BIb, BI, b, m, m);
         erase_epsilons_vector(BIb, m);
 
-        printf("\nBIb:\n");
-        for (i = 0; i < m; i++)
-            printf(" %6.2lf ", BIb[i]);
-        printf("\n");
-
-
-        copy_submatrix(D, A_aux,
-            0, m, m, n - m);
-
-        printf("D:\n");
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n - m; j++)
-                printf(" %6.2lf ", D[i][j]);
-            printf("\n");
-        }
-
+        copy_submatrix(D, A_aux, 0, m, m, n - m); 
 
         compute_cb_cd();
-
-        printf("\ncb:\n");
-        for (i = 0; i < m; i++)
-            printf(" %6.2lf ", cb[i]);
-        printf("\n");
-
-        printf("\ncd:\n");
-        for (i = 0; i < (n - m); i++)
-            printf(" %6.2lf ", cd[i]);
-        printf("\n");
-
 
         vector_matrix_mult(cbBI, cb, BI, m, m);
         erase_epsilons_vector(cbBI, m);
 
-        printf("\ncbBI:\n");
-        for (i = 0; i < m; i++)
-            printf(" %6.2lf ", cbBI[i]);
-        printf("\n");
-
-
-        vector_matrix_mult(cbBID, cbBI, D,
-            m, n - m);
+        vector_matrix_mult(cbBID, cbBI, D, m, n - m);
         erase_epsilons_vector(cbBID, n - m);
 
-
-        printf("\ncbBID:\n");
-        for (i = 0; i < (n - m); i++)
-            printf(" %6.2lf ", cbBID[i]);
-        printf("\n");
-
-
-
-        vector_subtract(rd, cd, cbBID,
-            n - m);
+        vector_subtract(rd, cd, cbBID, n - m);
         erase_epsilons_vector(rd, n - m);
 
         printf("\nrd( cd - cbBID ):\n");
-        for (i = 0; i < (n - m); i++)
+        for(i = 0; i < (n - m); i++)
             printf(" %6.2lf ", rd[i]);
         printf("\n\n");
 
-
         min_value = find_min_value(rd, n - m);
-        if (min_value >= -1e-9) // בדיקה עם אפסילון ליתר ביטחון 
+        
+        if (min_value >= -0.000000001) {
             optimal_flag = 1;
+        }
         else
         {
-
-
-            best_k = -1;
-            min_score = 0.0;
+            // --- Steepest Edge Algorithm ---
+            
+            best_k_index = -1;
+            min_steepest_val = 1e30; 
 
             for (k = 0; k < (n - m); k++) {
-
-                if (rd[k] < -1e-9) {
-
-                    // חישוב הנורמה בריבוע של העמודה המתאימה d_k
-                    // העמודה d_k נמצאת במטריצה BIA_aux החל מעמודה m
-                    col_idx = m + k;
-                    norm_sq = 0.0;
-
+                
+                // Check only improving variables
+                if (rd[k] < -0.000000001) {
+                    
+                    current_norm_sq = 0.0;
+                    // Calculate norm of column vector (d_k)
                     for (i = 0; i < m; i++) {
-                        double val = BIA_aux[i][col_idx];
-                        norm_sq += val * val;
+                        double val = BIA_aux[i][m + k];
+                        current_norm_sq += val * val;
                     }
+                    
+                    current_norm = sqrt(current_norm_sq); 
+                    if (current_norm < 1e-9) current_norm = 1e-9;
 
-                    if (norm_sq < 1e-9) norm_sq = 1e-9;
+                    // Steepest Edge Rule: rd / ||d||
+                    current_ratio = rd[k] / current_norm;
 
-                    current_score = rd[k] / norm_sq;
-
-                    if (best_k == -1 || current_score < min_score) {
-                        min_score = current_score;
-                        best_k = k;
+                    if (current_ratio < min_steepest_val) {
+                        min_steepest_val = current_ratio;
+                        best_k_index = k;
                     }
                 }
             }
 
-            if (best_k != -1) {
-                enter_id = d[best_k];
+            if (best_k_index != -1) {
+                // Map local index to global variable ID
+                enter_id = d[m + best_k_index];
+            } else {
+                enter_id = find_most_negative();
             }
-            else {
-                optimal_flag = 1;
-            }
+            
+            // --- End Algorithm ---
 
+            exiting_id = find_exiting_id(BIA_aux, BIb, enter_id, n, m);
+            
+            int exiting_val = (exiting_id >= 0 && exiting_id < m) ? basis[exiting_id] : -1;
 
-            exiting_id =
-                find_exiting_id(BIA_aux, BIb,
-                    enter_id, n, m);
             printf("\nenter_id  = %d,  exiting_id = %d,"
-                " d[exiting_id] = %d\n",
-                enter_id, exiting_id,
-                d[exiting_id]);
+                   " d[exiting_id] = %d\n", 
+                   enter_id, exiting_id, exiting_val);
 
-            printf("pivot: enter_id = %d, exiting_id = %d\n", enter_id,
-                d[exiting_id]);
-
-
-
+            printf("pivot: enter_id = %d, exiting_id = %d\n", enter_id, exiting_val);
 
             basis[exiting_id] = enter_id;
+            
             printf("\nbasis3:\n");
-            for (i = 0; i < m; i++)
+            for(i = 0; i < m; i++)
                 printf(" %d ", basis[i]);
             printf("\n");
 
-        }
-
-    }
-
-}
+        } // else
+    } // while
+} // simplex_algorithm
